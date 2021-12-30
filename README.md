188d0f56b1d7f829ccbe629a1a7d43aaf5e88026
unknown <haroonraizada@gmail.com>
2021-12-30 19:46:43 +0530

    last commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ruleEngine
 Business Rule Engine program
+This is a generic rule Engine Program.
+With each new rule corresponds to a java file in rule package.
+
+Steps to add a new rule.
+
+Create a java class file in rule package and extends RuleExecutor interface or a custom abstract class which accomodates 
+a common functionality between two rules. Like we extends AbstractEmailNotification class which has common functionality of sending email for membership and upgrade rules.
+
+Add all pre requisites in executePreRule method and rule logic in executeRule.
+
+Second step is to add switch case in TriggerRuleEngine file with newly added rule name.
+
+Only above two steps will add a new rule.
+
+To run and test this program follow below steps:
+
+1. Clone this project and run mvn clean install. It will run all junit test cases, written for each rule.
+
+2. Then execute mvn exec:java@someID -Dexec.args=<RuleName>. E.g  try with "Book","Physical Product" to test our rules.
+
+mvn exec:java@someID -Dexec.args="Video" and see in console the expected output.
+
+3. Below are the names of the rules:
+
+Book, Physical Product,Membership,Upgrade,Video
+
--- src/test/java/triggerRules/TriggerRuleEngineTest.java
+++ src/test/java/triggerRules/TriggerRuleEngineTest.java
@@ -18,95 +18,92 @@ import model.RoyalityModel;
 public class TriggerRuleEngineTest {
 
 	private final static ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	PrintStream printStream=new PrintStream(outContent,true);
+	PrintStream printStream = new PrintStream(outContent, true);
 
-	public static Logger logger=Logger.getLogger("TriggerRuleEngineTest");
-	//Rule for Main Class. If no input passed
-	 @BeforeClass
-	    public static void setUpStreams() {
-		   System.setErr(new PrintStream(outContent));
-	        
-	    }
+	public static Logger logger = Logger.getLogger("TriggerRuleEngineTest");
 
-	    @AfterClass
-	    public static void cleanUpStreams() {
-	        System.setErr(null);
-	    }
+	@BeforeClass
+	public static void setUpStreams() {
+		System.setErr(new PrintStream(outContent));
+
+	}
+
+	@AfterClass
+	public static void cleanUpStreams() {
+		System.setErr(null);
+	}
+
+	// Test case to test Book Rule
 	@Test
 	public void executeRuleTestBook() throws Exception {
-		String[] inputParams= {"Book"};
+		String[] inputParams = { "Book" };
 		TriggerRuleEngine.main(inputParams);
 		Boolean checkCommision = outContent.toString().contains("commission has been set for the agent");
 		Boolean isTrue = outContent.toString().contains("created a duplicate packing slip for the royalty department");
 		assertEquals(true, isTrue && checkCommision);
-		
-		
+
 	}
-	
+
+	// Test case to test Physical Product Rule
 	@Test
 	public void executeRuleTestPhysicalProduct() throws Exception {
-		
-		String[] inputParams= {"Physical Product"};
+
+		String[] inputParams = { "Physical Product" };
 		TriggerRuleEngine.main(inputParams);
 		Boolean checkCommision = outContent.toString().contains("commission has been set for the agent");
 		Boolean isTrue = outContent.toString().contains("generate shipping slip for physical product");
-		
+
 		assertEquals(true, isTrue && checkCommision);
-		
-		
+
 	}
-	
+
+	// Test case to test Membership Rule
 	@Test
 	public void executeRuleTestMembership() throws Exception {
-		
-		String[] inputParams= {"Membership"};
+
+		String[] inputParams = { "Membership" };
 		TriggerRuleEngine.main(inputParams);
 		Boolean checkEmail = outContent.toString().contains(" Sent email successfully....");
 		Boolean isTrue = outContent.toString().contains("Activate that membership");
-		
+
 		assertEquals(true, isTrue && checkEmail);
-		
-		
+
 	}
-	
+
+	// Test case to test Membership upgrade Rule
 	@Test
 	public void executeRuleTestUpgrade() throws Exception {
-		
-		String[] inputParams= {"Upgrade"};
+
+		String[] inputParams = { "Upgrade" };
 		TriggerRuleEngine.main(inputParams);
 		Boolean checkEmail = outContent.toString().contains(" Sent email successfully....");
 		Boolean isTrue = outContent.toString().contains("Upgrade that membership");
-		
+
 		assertEquals(true, isTrue && checkEmail);
-		
-		
+
 	}
-	
+
+	// Test case to test Video Rule
 	@Test
 	public void executeRuleTestVideo() throws Exception {
-		
-		String[] inputParams= {"Video"};
+
+		String[] inputParams = { "Video" };
 		TriggerRuleEngine.main(inputParams);
-		Boolean isTrue = outContent.toString().contains("add a free “First Aid” video to the packing slip (the result of a court decision in 1997)");
-		
-		assertEquals(true, isTrue );
-		
-		
+		Boolean isTrue = outContent.toString()
+				.contains("add a free “First Aid” video to the packing slip (the result of a court decision in 1997)");
+
+		assertEquals(true, isTrue);
+
 	}
-	
-	
+
 	@Test
-	public  void testexecuteRuleOneWithNoorInvalidInput() {
-	try {
-	TriggerRuleEngine.main(null);
-	}catch (Exception e) {
-		assertEquals(e.getLocalizedMessage(),"invalid input passed");
+	public void testexecuteRuleOneWithNoorInvalidInput() {
+		try {
+			TriggerRuleEngine.main(null);
+		} catch (Exception e) {
+			assertEquals(e.getLocalizedMessage(), "invalid input passed");
+		}
+
 	}
-	
-	
-	
-	}
-	
-	
-	
+
 }
